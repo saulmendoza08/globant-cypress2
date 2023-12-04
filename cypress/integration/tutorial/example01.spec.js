12ad2da440beece52cfd0d95e90d103cba5f0d4d
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-12-04 20:42:11 -0300

    cypress priomer commit

---
+++ cypress/integration/tutorial/example01.spec.js
@@ -0,0 +1,12 @@
+context('Example_01',()=>{
+    //primero se define la url, en este caso toma la baseUrl+ cy.visit.
+    beforeEach(()=>{
+        cy.visit('/');
+    })
+
+    //haciendo el caso de prueba
+    it('Test #1',()=>{
+        
+    })
+
+})
