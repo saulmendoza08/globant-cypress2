2c50e2b49f78356cb3024941e058eaa2c4706143
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-12-04 20:52:22 -0300

    primera automatizacion realizada , ejemplo1

--- cypress/integration/tutorial/example01.spec.js
+++ cypress/integration/tutorial/example01.spec.js
@@ -6,7 +6,12 @@ context('Example_01',()=>{
 
     //haciendo el caso de prueba
     it('Test #1',()=>{
-        
+        cy.get('.new-todo').type("My long task #1{enter}");
+        cy.get('.new-todo').type("My long task #2{enter}");
+        cy.contains('Completed').click();
+        cy.contains('Active').click();
+         
+
     })
 
 })
