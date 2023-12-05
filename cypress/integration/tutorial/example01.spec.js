fee1d6c2310ab7e17aa33e6fba147a1a361a362a
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-12-04 21:25:42 -0300

    tercer ejemplo, selectores usando text content

--- cypress/integration/tutorial/example01.spec.js
+++ cypress/integration/tutorial/example01.spec.js
@@ -10,8 +10,22 @@ context('Example_01',()=>{
         cy.get('.new-todo').type("My long task #2{enter}");
         cy.contains('Completed').click();
         cy.contains('Active').click();
-         
+    })
 
+    it('Test #2 : Select using CSS selectors',()=>{
+        cy.get('.new-todo').type("My long task #1{enter}");
+        cy.get('.new-todo').type("My long task #2{enter}");
+        cy.get(':nth-child(2) > .view > .toggle').click();
+        cy.contains('Completed').click();
+    })
+
+    it('Test #3 : Select using Text content',()=>{
+        cy.get('.new-todo').type("My long task #1{enter}");
+        cy.get('.new-todo').type("My long task #2{enter}");
+        cy.get('label:contains("My long task #1")')
+            .parent().find('.toggle')
+            .click();
+        cy.contains('Completed').click();
     })
 
 })
